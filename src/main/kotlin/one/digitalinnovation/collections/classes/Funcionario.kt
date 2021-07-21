f6fdb6e184cecbc09136b7ed399777269e4cd9be
Lucca Raphael <luccaraphae@gmail.com>
2021-07-21 12:21:24 -0300

    c08- Atualizando classe Funcionario ao subscrever função toString para melhor apresentação dos dados

--- src/main/kotlin/one/digitalinnovation/collections/classes/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/collections/classes/Funcionario.kt
@@ -3,9 +3,17 @@ package one.digitalinnovation.collections.classes
 data class Funcionario(
     val nome:String,
     val idade:Int,
-    val telefone:Int,
+    val telefone:String,
     val salario:Double
-)
-
+) {
+    //subescreve a função toString para apresentar os dados de forma concatenada
+    override fun toString(): String =
+        """
+            Nome:     $nome
+            Idade:    $idade
+            Telefone: $telefone
+            Salário:  $salario
+        """.trimIndent()
+}
 
 
